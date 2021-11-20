140fc9ccde1e78ee84b6ef78d8388f1d18f8c3b1
devkwonsehoon <devkwonsehoon@naver.com>
2021-11-21 06:16:12 +0900

    add: USER API 완성

---
+++ functions/api/routes/user/userPOST.js
@@ -0,0 +1,30 @@
+const functions = require('firebase-functions');
+const { success, fail } = require('../../../lib/util');
+const sc = require('../../../constants/statusCode');
+const rm = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { userDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  //   const profileImage = req.file;
+  const { code, username, userId } = req.body;
+  const url = 'https://user-images.githubusercontent.com/54793607/142727985-d8aaaad5-f7b1-47bb-91e5-fb4936db1b74.png';
+
+  if (!username) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+    const user = await userDB.addUser(client, code, userId, username, url);
+
+    res.status(sc.OK).send(success(sc.OK, rm.CREATE_ONE_USER_SUCCESS, user));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
--- functions/db/user.js
+++ functions/db/user.js
@@ -4,52 +4,26 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const getAllUsers = async (client) => {
   const { rows } = await client.query(
     `
-    SELECT * FROM "user" u
+    SELECT * FROM "profile" p
     WHERE is_deleted = FALSE
     `,
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-const updateUser = async (client, username, phone, userId) => {
-  const { rows: existingRows } = await client.query(
-    `
-    SELECT * FROM "user"
-    WHERE id = $1
-       AND is_deleted = FALSE
-    `,
-    [userId],
-  );
-
-  if (existingRows.length === 0) return false;
-
-  const data = _.merge({}, convertSnakeToCamel.keysToCamel(existingRows[0]), { username, phone });
-
+const addUser = async (client, code, userId, username, imageUrl) => {
   const { rows } = await client.query(
     `
-    UPDATE "user" u
-    SET username = $1, phone = $2, updated_at = now()
-    WHERE id = $3
-    RETURNING * 
-    `,
-    [data.username, data.phone, userId],
-  );
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-const addUser = async (client, email, username, phone, idFirebase) => {
-  const { rows } = await client.query(
-    `
-    INSERT INTO mockup_user
-    (email, username, phone id_firebase)
+    INSERT INTO profile
+    (code, user_id, username, profile_image)
     VALUES
-    ($1, $2, $3)
+    ($1, $2, $3, $4)
     RETURNING *
     `,
 
-    [email, username, phone, idFirebase],
+    [code, userId, username, imageUrl],
   );
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-module.exports = { getAllUsers };
+module.exports = { getAllUsers, addUser };
