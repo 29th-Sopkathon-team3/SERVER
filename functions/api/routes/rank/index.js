06fabf385e8faad6bbcaaa722361f6e2bfffa5ff
devkwonsehoon <devkwonsehoon@naver.com>
2021-11-21 00:40:10 +0900

    add: API 초기 설정

---
+++ functions/api/routes/rank/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', require('./rankPOST')); // 랭킹 공동체 생성
+router.get('/', require('./rankGET')); // 공동체 랭킹 조회
+router.put('/', require('./rankEnterPUT')); // 유저 입장
+router.get('/:rankId/:userId', require('./rankDetailGET')); // 유저 랭킹 조회
+router.put('/:rankId/:userId', require('./rankPUT')); // 유저 랭킹 업데이트
+
+module.exports = router;
---
+++ functions/db/rank.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+
+const getAllUsers = async (client) => {
+  const { rows } = await client.query(
+    `
+    SELECT * FROM "user" u
+    WHERE is_deleted = FALSE
+    `,
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { getAllUsers };
